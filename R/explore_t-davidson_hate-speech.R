c0cfbf87e06aa14131cc11a3d6cf05de95c3682f
Jim Tyhurst <jim@tyhurst.com>
2017-11-18 10:00:36 -0800

    Summarizes files in other projects.

---
+++ R/explore_t-davidson_hate-speech.R
@@ -0,0 +1,45 @@
+library(readr)
+library(dplyr)
+library(ggplot2)
+
+# Summarizes data from file:
+# https://raw.githubusercontent.com/t-davidson/hate-speech-and-offensive-language/master/data/labeled_data.csv
+# in repository:
+# https://github.com/t-davidson/hate-speech-and-offensive-language
+#
+# See:
+# Thomas Davidsoon, Dana Warmsley, Michael Macy, and Ingmar Weber. 2017.
+# "Automated Hate Speech Detection and the Problem of Offensive Language".
+# Proceedings of the 11th International AAAI Conference on Web and Social Media. ICWSM '17, Montreal, Canada.
+# https://aaai.org/ocs/index.php/ICWSM/ICWSM17/paper/view/15665
+
+#' @export
+explore_t_davidson_hate_speech <- function() {
+  encoded_tweets <- readr::read_csv("https://raw.githubusercontent.com/t-davidson/hate-speech-and-offensive-language/master/data/labeled_data.csv")
+  names(encoded_tweets)[1] <- "seq_number"
+
+  # Histogram of tweet lengths
+  encoded_tweets %>%
+    mutate(tweet_length = nchar(tweet)) %>%
+    ggplot(aes(x = tweet_length)) +
+    geom_histogram(binwidth = 1) +
+    scale_x_continuous(name = "Number of characters in tweet",
+                       limits = c(0, 300),
+                       expand = c(0, 0)) +
+    ylab("Number of tweets") +
+    ggtitle("Length of tweets (data: labeled_data.csv)") %>%
+    print()
+
+  # Bar chart of the number of tweets in each class
+  encoded_tweets %>%
+    dplyr::group_by(class) %>%
+    dplyr::summarize(total = n()) %>%
+    ggplot(aes(x = factor(class), y = total)) +
+    geom_bar(stat = "identity") +
+    geom_text(aes(label = total, vjust = -0.25)) +
+    scale_x_discrete(labels = c("hate speech", "offensive language", "neither")) +
+    xlab("class") +
+    ylab("quantity") +
+    ggtitle("Number of tweets in each class (data: labeled_data.csv)") %>%
+    print()
+}
---
+++ R/explore_zeerakw_hate-speech.R
@@ -0,0 +1,38 @@
+library(readr)
+library(dplyr)
+library(ggplot2)
+
+# Summarizes data from file:
+# https://github.com/ZeerakW/hatespeech/blob/master/NAACL_SRW_2016.csv
+# in repository:
+# https://github.com/ZeerakW/hatespeech
+#
+# See:
+# Zeerak Waseem and Dirk Hovy. 2016.
+# "Hateful Symbols or Hateful People? Predictive Features for Hate Speech Detection on Twitter", pp. 88-93.
+# Proceedings of the NAACL Student Research Workshop, June 2016.
+# San Diego, CA: Association for Computational Linguistics.
+# http://www.aclweb.org/anthology/N16-2013
+
+#' @export
+explore_zeerakw_hate_speech <- function() {
+  encoded_tweets <-
+    read_csv(
+      "https://raw.githubusercontent.com/ZeerakW/hatespeech/master/NAACL_SRW_2016.csv",
+      col_names = c("tweet_id", "rating"),
+      col_types = list(col_character(), col_character())
+    ) %>%
+    mutate(rating = as.factor(rating))
+
+  encoded_tweets %>%
+    group_by(rating) %>%
+    summarize(total_tweets = n()) %>%
+    ggplot(aes(x = rating, y = total_tweets)) +
+    geom_bar(stat = "identity") +
+    geom_text(aes(label = total_tweets, vjust = -0.25)) +
+    xlab("Rating") +
+    ylab("Quantity") +
+    ggtitle("Number of tweets by rating (data: NAACL_SRW_2016.csv)") %>%
+    print()
+
+}
